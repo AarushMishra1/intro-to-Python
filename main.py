d99cb1b46b1b165cd5db0efaa91837ca3efceb44
AarushMishra1 <a.mishra123@hotmail.com>
2025-04-12 21:53:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print ("Welcome To Python")
+a = 10
+print (a)
+print ("Hello, How are you?")
+print ("I am fine, Thank you")
+print ("the value of a list is", a)
+
+import keyword
+print (keyword.kwlist)
